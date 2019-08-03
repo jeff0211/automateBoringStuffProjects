c12c0229b8633954498d3dc47a186f15d75d8957
jeff0211 <53135700+jeff0211@users.noreply.github.com>
2019-08-03 22:51:43 +0800

    Update fillGap.py

--- Chapter 9/fillGap.py
+++ Chapter 9/fillGap.py
@@ -1,27 +1,39 @@
 import os, re, shutil
 
-# C:\Users\User\Documents\myPythonProjects\AutomateBoringStuffsProjects\Chapter_9\Ch9_Test
-srcFolder = r'C:\Users\User\Documents\myPythonProjects\AutomateBoringStuffsProjects\Chapter_9\Ch9_Test'
-    # input('Enter source folder to search from:\n')
-# C:\Users\User\Documents\myPythonProjects\AutomateBoringStuffsProjects\Chapter_9\Ch9_Test\Ch9_NewCopy
-desFolder = r'C:\Users\User\Documents\myPythonProjects\AutomateBoringStuffsProjects\Chapter_9\Ch9_Test\Ch9_NewCopy'
-    # input('Enter destination folder to move and rename:\n')
+src_fdr = input('Enter source folder to search from:\n')
+des_fdr = input('Enter destination folder to rename to:\n')
+prefix_pos, prefix_len =input('Prefix starts at (Beginning/End) of file name?: '), str(len(input('Prefix starts with: ')))
 
-prefixCount = 1
+prefix_start_regex = re.compile(r'\b\d{' + prefix_len + r'}(\D+\.\w+)')  # Search prefix at beginning of file name.
+prefix_end_regex = re.compile(r'(\w+\D)\d{' + prefix_len + r'}(\.\w+)')  # Search prefix at end of file name.
+prefix_count = 1
 list_files = []
 
-fileRegex = re.compile(r'(\w+)(\d{3})(\.\w+)')
-for folders, subfolders, files in os.walk(srcFolder):
+for folders, subfolders, files in os.walk(src_fdr):
     for file in files:
-        fileObj1 = fileRegex.search(file)
-        if fileObj1 != None:
-            list_files.append(file)
+        if prefix_pos == 'Beginning':  # Find files with prefix starting & append to a list var.
+            fileObj = prefix_start_regex.search(file)
+            if fileObj != None:
+                list_files.append(file)
+        elif prefix_pos == 'End':  # Find files with prefix ending & append to a list var.
+            fileObj = prefix_end_regex.search(file)
+            if fileObj != None:
+                list_files.append(file)
 
-for each in sorted(list_files):
-    fileObj2 = fileRegex.search(each)
-    if fileObj2 != None:
-            prefix = str(prefixCount).zfill(3)
-            newFileName = fileObj2.group(1) + prefix + fileObj2.group(3)
-            prefixCount += 1
-            print(newFileName)
-            shutil.move(os.path.join(srcFolder, each), os.path.join(desFolder, newFileName))
+for each in sorted(list_files):  # Sort the list in ascending order.
+    if prefix_pos == 'Beginning':  # Rename prefix at the beginning of file name.
+        fileObj = prefix_start_regex.search(each)
+        if fileObj != None:
+            new_prefix = str(prefix_count).zfill(int(prefix_len))
+            new_filename = new_prefix + fileObj.group(1)
+            prefix_count += 1
+            shutil.move(os.path.join(src_fdr, each), os.path.join(des_fdr, new_filename))
+    elif prefix_pos == 'End':  # Rename prefix at the end of file name.
+        fileObj = prefix_end_regex.search(each)
+        if fileObj != None:
+            new_prefix = str(prefix_count).zfill(int(prefix_len))
+            new_filename = fileObj.group(1) + new_prefix + fileObj.group(2)
+            prefix_count += 1
+            shutil.move(os.path.join(src_fdr, each), os.path.join(des_fdr, new_filename))
+            
+print('Prefixes for files are renamed in running order.')
