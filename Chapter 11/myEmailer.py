ed6e595a56b9f0d8e5f132005aee17a09e838ca9
jeff0211 <53135700+jeff0211@users.noreply.github.com>
2019-08-18 15:19:10 +0800

    Update myEmailer.py

--- Chapter 11/myEmailer.py
+++ Chapter 11/myEmailer.py
@@ -35,7 +35,8 @@ else:
     contentElem = browser.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/div[1]/div[3]/div[2]/div/div[3]/div['
                                                 '1]/div/div/div/div[1]/div[2]/div')
     contentElem.send_keys(emailMsg)
-    sendElem = browser.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/div[1]/div[3]/div[2]/div/div[3]/div[1]/div/div/div/div[1]/div[4]/div[2]/div[1]/button[1]/div')
+    sendElem = browser.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/div[1]/div[3]/div[2]/div/div[3]/div[1]/'
+                                             'div/div/div/div[1]/div[4]/div[2]/div[1]/button[1]/div')
     sendElem.click()
     print('Your email is sent to {}.'.format(emailRecep))
     browser.quit()
