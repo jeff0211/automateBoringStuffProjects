326ac0bdd60802837981020081431cfa6fda56e5
jeff0211 <53135700+jeff0211@users.noreply.github.com>
2019-09-01 00:49:48 +0800

    Update multiplicationTable.py

--- Chapter 12/multiplicationTable.py
+++ Chapter 12/multiplicationTable.py
@@ -1,5 +1,6 @@
 import openpyxl, sys, os
 from openpyxl.utils import get_column_letter
+from openpyxl.styles import Font
 
 n = int(sys.argv[1])
 cwd = os.chdir(input('Save to Directory: '))
@@ -9,6 +10,8 @@ sheet = wb.active
 for i in range(1, n + 1):
     sheet.cell(row=1, column=i + 1).value = i
     sheet.cell(row=i + 1, column=1).value = i
+    sheet.cell(row=1, column=i + 1).font = Font(bold=True)
+    sheet.cell(row=i + 1, column=1).font = Font(bold=True)
 
 for eachRow in range(1, n + 1):
     for eachCol in range(2, int(sheet.max_column) + 1):
