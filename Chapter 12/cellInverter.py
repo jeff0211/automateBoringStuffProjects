d5a3ec40a3d558a51c953cfc7320e233349cc5ff
jeff0211 <53135700+jeff0211@users.noreply.github.com>
2019-09-05 22:04:09 +0800

    Update cellInverter.py

--- Chapter 12/cellInverter.py
+++ Chapter 12/cellInverter.py
@@ -9,4 +9,5 @@ for x in range(1, sheet.max_row + 1):
     for y in range(1, sheet.max_column + 1):
         new_sheet.cell(row=y, column=x).value = sheet.cell(row=x, column=y).value
 
+print('The data has been inverted in a new sheet.')
 wb.save('invertersample2.xlsx')
